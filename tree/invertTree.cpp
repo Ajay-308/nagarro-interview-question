c99068799fa2296febba115a9226fc5231ad0d4e
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-09-04 22:36:52 +0530

    invertTree

---
+++ tree/invertTree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+
+        TreeNode * temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->right);
+        invertTree(root->left);
+
+        return root;
+        
+    }
+};
