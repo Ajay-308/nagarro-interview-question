58b7b8b88795cd573db42c1d6f73d3b21caa7cac
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-09-05 09:41:23 +0530

    final commit

--- queue_using_stack.cpp
+++ queue_using_stack.cpp
@@ -1,3 +1,40 @@
-ye bhi implement kar liyo 
+class MyQueue {
+public:
+    stack<int> s;
+    stack<int> p;
 
-2 stack method se
+    // queue --> 1,2,3,4,5
+    // stack1--> 5,4,3,2,1
+    // stack2 pop--> 1,2,3,4,5
+    MyQueue() {}
+
+    void push(int x) {
+        s.push(x);
+    }
+
+    int pop() {
+        if (p.empty()) {
+            while (!s.empty()) {
+                p.push(s.top());
+                s.pop();
+            }
+        }
+        int t = p.top();
+        p.pop();
+        return t;
+    }
+
+    int peek() {
+        if (p.empty()) {
+            while (!s.empty()) {
+                p.push(s.top());
+                s.pop();
+            }
+        }
+        return p.top();
+    }
+
+    bool empty() {
+        return s.empty() && p.empty();
+    }
+};
