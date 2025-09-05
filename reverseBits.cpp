3989eac73d78c8eec01c181c58c11aad086e1ebb
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-09-05 09:16:30 +0530

    reverseBits

---
+++ reverseBits.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            int bit = (n >> i) & 1;            // extract i-th bit
+            result = (result << 1) | bit;      // shift result left, append bit
+        }
+        
+        return result;
+    }
+};
+// time complexity : 0(1) as loop run fixed 32 times
+// space complexity : O(1) no extra space used
+
+// ✅ Example dry run:
+// If n = 13 (0b00000000000000000000000000001101)
+
+// Iter 0: bit = 1 → result = 1
+
+// Iter 1: bit = 0 → result = 10 (binary)
+
+// Iter 2: bit = 1 → result = 101 (binary)
+
+// Iter 3: bit = 1 → result = 1011 (binary)
+
+// Rest bits → 0’s → final = 10110000000000000000000000000000
